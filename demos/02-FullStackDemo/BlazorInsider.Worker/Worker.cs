9b2bbbb9100fffde37a21061084ffa32035ffb0f
Scott Dockendorf <scott@dockendorf.org>
2019-09-11 14:56:13 -0500

    Refactored UpdateOrder method into API and wired up WindowsService to use API method exclusively

--- demos/02-FullStackDemo/BlazorInsider.Worker/Worker.cs
+++ demos/02-FullStackDemo/BlazorInsider.Worker/Worker.cs
@@ -13,12 +13,12 @@ namespace BlazorInsider.Worker
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
-        private DatabaseService _databaseService;
+        //private DatabaseService _databaseService;
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
-            _databaseService = new DatabaseService();
+            //_databaseService = new DatabaseService();
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -31,17 +31,44 @@ namespace BlazorInsider.Worker
                     AppContext.SetSwitch(
                     "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport",
                     true);
-                    HttpClient httpClient = new HttpClient();
+
+                    var httpClient = new HttpClient();
                     httpClient.BaseAddress = new Uri("http://127.0.0.1:8194");
 
                     var grpcClient = GrpcClient.Create<OrdersManager.OrdersManagerClient>(httpClient);
 
                     OrderRequest request = new OrderRequest();
                     OrderReply result = await grpcClient.GetNewOrderAsync(request);
+
                     if (result.OrderId != 0)
                     {
-                        _databaseService.UpdateOrder(result.OrderId);
-                        _logger.LogInformation($"Order with id {result.OrderId} has been processed");
+                        var updateRequest = new UpdateOrderRequest()
+                        {
+                            OrderID = result.OrderId
+                        };
+
+                        try
+                        {
+                            UpdateOrderResponse updateResponse =
+                                await grpcClient.UpdateOrderAsync(updateRequest);
+
+                            if (updateResponse.Success)
+                            {
+                                _logger.LogInformation($"Order with id {result.OrderId} has been processed");
+                            }
+                            else
+                            {
+                                _logger.LogInformation($"Order with id {result.OrderId} was not updated and returned error {updateResponse.ErrorMessage}");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            var msg = (ex.InnerException != null)
+                                ? $"{ex.Message} {ex.InnerException.Message} {ex.StackTrace}"
+                                : $"{ex.Message} {ex.StackTrace}";
+                            _logger.LogInformation($"Order with id {result.OrderId} was not updated and returned error {msg}");
+
+                        }
                     }
                     else
                     {
